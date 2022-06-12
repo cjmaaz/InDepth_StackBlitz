80569439cf39f86a6ed664990745a8b856a0e47a
cjmaaz <cjmaaz@outlook.com>
2022-06-12 21:29:10 +0530

    2 files modified, 1 added

---
+++ basics.js
@@ -0,0 +1,23 @@
+let name = 'Getting Name';
+let age = 26;
+
+(async () => {
+  let body = await fetch('https://dummyjson.com/users/1');
+  let resp = await body.json();
+  console.log(JSON.stringify(resp));
+  name = resp.firstName;
+  age = resp.age;
+  console.log(name);
+  updateDetails();
+})();
+
+let updateDetails = () => {
+  let details = document.createElement('div');
+
+  details.innerHTML = `<h1>${name}</h1>
+                        <h3>${age}</h3>`;
+
+  let app = document.querySelector('#app').append(details);
+};
+
+// Remove Await
